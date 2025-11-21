5b16256627b2d8a50941c44b721ed4af6d34341b
Erick Marques <erickmarques2564@gmail.com>
2025-11-21 05:07:08 -0300

    feat(device-backend): adjust sendHeartbeat to send random voltage with small deviation around 220

--- backend modules/device/src/main/java/com/gridguard/device/service/DeviceService.java
+++ backend modules/device/src/main/java/com/gridguard/device/service/DeviceService.java
@@ -9,18 +9,20 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Random;
 
 @Service
 @EnableScheduling
 public class DeviceService {
 
     private final RestTemplate http = new RestTemplate();
+    private final Random rand = new Random();
 
     @Scheduled(fixedRate = 2000)
     public void sendHeartbeat() {
         DeviceStatusDTO dto = new DeviceStatusDTO(
                 "device-1",
-                220,
+                220 + (rand.nextDouble() - 0.5) * 5,
                 0,
                 Instant.now().truncatedTo(ChronoUnit.MILLIS),
                 "signature",
