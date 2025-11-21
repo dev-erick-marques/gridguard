4f27a202ec956f511e2d412839237b45caff59df
Erick Marques <erickmarques2564@gmail.com>
2025-11-21 04:53:49 -0300

    refactor(device-backend): update controller methods to accept CommandRequestDTO and return ResponseEntity

--- backend modules/device/src/main/java/com/gridguard/device/controller/DeviceController.java
+++ backend modules/device/src/main/java/com/gridguard/device/controller/DeviceController.java
@@ -1,6 +1,9 @@
 package com.gridguard.device.controller;
 
+import com.gridguard.device.dto.CommandRequestDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeviceController {
 
     @PostMapping("/shutdown")
-    public void safeShutdown(){
-        System.out.println("Safe shutdown initiated");
+    public ResponseEntity<String> safeShutdown(@RequestBody CommandRequestDTO dto) {
+        System.out.println(dto);
+        return ResponseEntity.ok("Safe shutdown initiated");
     }
 
     @PostMapping("/restart")
-    public void safeRestart(){
-        System.out.println("Safe restart initiated");
-
+    public ResponseEntity<String> safeRestart(@RequestBody CommandRequestDTO dto) {
+        System.out.println(dto);
+        return ResponseEntity.ok("Safe restart initiated");
     }
 }
